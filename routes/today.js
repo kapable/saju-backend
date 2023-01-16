baf801b59e2e64e03a5de52d1118b900b2d3b612
kapable <skjoldenstore@gmail.com>
2023-01-16 12:13:16 +0900

    cors for all origins and test with RDS DB

--- routes/today.js
+++ routes/today.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const { Op, where } = require('sequelize');
+const { Mansedata } = require('../models');
+
+const leftPad = (value) => {
+    if (value >= 10) {
+        return value;
+    }
+    return `0${value}`;
+};
+
+const toStringByFormatting = (source, delimiter = '') => {
+    const year = source.getFullYear();
+    const month = leftPad(source.getMonth() + 1);
+    const day = leftPad(source.getDate());
+
+    return [year, month, day].join(delimiter);
+};
 
 router.get('/total', async(req, res, next) => {
     try {
-        res.status(200).send('GOOd');
+        const todayFormatted = toStringByFormatting(new Date());
+        const day_info = Mansedata.findOne({
+            where: { no: todayFormatted }
+        })
+        res.status(200).json(day_info);
     } catch (error) {
         console.error(error);
         next(error);
