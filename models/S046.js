b6d2c5652bd4807b2b7833d9be9b15e85b0c1df8
kapable <skjoldenstore@gmail.com>
2023-02-23 11:45:35 +0900

    add S045 S046 S047 for lifeInterpreting

---
+++ models/S046.js
@@ -0,0 +1,49 @@
+const DataTypes = require('sequelize');
+const { Model } = DataTypes;
+
+module.exports = class S046 extends Model {
+    static init(sequelize) {
+        return super.init({ 
+            DB_check: {
+                type: DataTypes.CHAR(2),
+                allowNull: true,
+            },
+            DB_data: {
+                type: DataTypes.TEXT,
+                allowNull: true,
+            },
+            DB_express: {
+                type: DataTypes.CHAR(10),
+                allowNull: true,
+            },
+            DB_num: {
+                type: DataTypes.CHAR(2),
+                allowNull: true,
+            },
+            DB_numerical: {
+                type: DataTypes.CHAR(2),
+                allowNull: true,
+            },
+            DB_title: {
+                type: DataTypes.TEXT,
+                allowNull: true,
+            },
+            createdAt: {
+                allowNull: true,
+                type: DataTypes.DATE
+            },
+            updatedAt: {
+                allowNull: true,
+                type: DataTypes.DATE
+            }
+        }, {
+            modelName: 'S046',
+            tableName: 'S046',
+            charset: 'utf8mb4',
+            collate: 'utf8mb4_general_ci',
+            sequelize
+        });
+    };
+    static associate(db) {
+    };
+};
